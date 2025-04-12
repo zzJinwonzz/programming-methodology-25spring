5d75d66c94c15e7810db2479fc3e7ade2ff4cd02
JinWon <boky618@snu.ac.kr>
2025-04-12 23:26:16 +0900

    assignment_fin

--- test_todo.cpp
+++ test_todo.cpp
@@ -57,6 +57,8 @@ TEST(TodoListTest, RemoveInvalidIndexThrows) {
 
     // TODO: Try removing at invalid indices
     // Example: negative index and out-of-bound index
+    EXPECT_THROW(todo.remove_task(-2), std::invalid_argument);
+    EXPECT_THROW(todo.remove_task(618), std::invalid_argument);
 
     todo.destroy();
 }
@@ -66,7 +68,12 @@ TEST(TodoListTest, AddTooManyTasksThrows) {
     todo.init();
 
     // TODO: Add exactly 32 tasks in a loop
+    for (int i=0; i<32; i++){
+        todo.add_task("A");
+    }
     // Then try adding a 33rd task and expect an exception
+    EXPECT_THROW(todo.add_task("33"), std::invalid_argument);
+
 
     todo.destroy();
 }
